f4e2460c90b07d3fc30b22ec38d028661e8d2700
sakhawathossensadi <newtonss919@gmail.com>
2022-09-14 23:55:16 +0600

    JS operators

---
+++ JS/Operators.js
@@ -0,0 +1,24 @@
+let p = 2;
+let q = 2;
+let r = p + q;
+console.log(r);
+
+let aa = 5;
+console.log(aa++);
+console.log(aa);
+
+let bb = 5;
+console.log(++bb);
+console.log(bb);
+
+// string
+
+let s = "Hello";
+let t = "world";
+let res = s + " " + t + "!";
+console.log(res)
+
+
+let pp = 10;
+console.log(typeof pp);
+
