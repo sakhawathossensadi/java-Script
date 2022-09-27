e4bcdc75c2bab1c14e18cf784a483b6a90722b5e
sakhawathossensadi <newtonss919@gmail.com>
2022-09-27 23:44:25 +0600

    JS function

---
+++ JS/Function.js
@@ -0,0 +1,22 @@
+function myFunction(p1, p2) {
+    let sum;
+    sum = p1 + p2;
+    console.log("Sum : ",sum);
+}
+
+myFunction(10,20);
+
+function multiply(a1, a2) {
+    return a1 * a2;
+}
+
+let mul = multiply(10,20);
+console.log("Result : ",mul);
+
+//from function if we return nothing JS return by default undefined
+function subtraction(val1, val2) {
+    console.log("subtraction");
+}
+
+let sub = subtraction(10,5);
+console.log("Sub : ",sub);
