b620e34db4a1080e181de8b91c66d2347467a4a9
sakhawathossensadi <newtonss919@gmail.com>
2022-09-29 01:17:46 +0600

    string details

--- JS/String.js
+++ JS/String.js
@@ -11,3 +11,24 @@ console.log(str2);
 
 let str3 = "Address : 58\\B\\2 West Rajabazar";
 console.log(str3);
+
+//String Object
+
+let st = "Sakhawat Hossen";
+let sp = new String("Sadi");
+console.log(typeof st);
+console.log(typeof sp);
+
+let s1 = "Sadi";
+let s2 = new String("Sadi");
+
+console.log(s1 == s2);
+// ===  check type and value
+console.log(s1 === s2);
+
+let a1 = new String("sadi");
+let a2 = new String("sadi");
+
+// Comparing two JavaScript objects always returns false.
+console.log(a1 == a2);
+console.log(a1 === a2);
