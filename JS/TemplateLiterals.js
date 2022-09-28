56956c4d029b388bb8289ce4530fdea67ba7633e
sakhawathossensadi <newtonss919@gmail.com>
2022-09-29 01:28:05 +0600

    JS TemplateLiterals

---
+++ JS/TemplateLiterals.js
@@ -0,0 +1,15 @@
+let stringValue = `Hello World!, It's JS`;
+console.log(stringValue);
+let strValue = `Hello 
+World!`;
+console.log(strValue);
+
+// Interpolation
+let country = "Bangladesh";
+let sentence = `I love ${country}`;
+console.log(sentence);
+
+let v1 = 10;
+let v2 = 20;
+let v3 = `Sum : ${v1 + v2}`;
+console.log(v3);
