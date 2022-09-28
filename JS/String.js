b583c7d6cca2b608fee194f00671a326dd418dc2
sakhawathossensadi <newtonss919@gmail.com>
2022-09-29 01:11:33 +0600

    JS string

---
+++ JS/String.js
@@ -0,0 +1,13 @@
+let string = "Sakhawat Hossen";
+console.log("String Length : ",string.length);
+
+//Escape Character
+
+let str1 = "We are the so-called \"Vikings\" from the north.";
+console.log(str1);
+
+let str2= 'It\'s alright.';
+console.log(str2);
+
+let str3 = "Address : 58\\B\\2 West Rajabazar";
+console.log(str3);
