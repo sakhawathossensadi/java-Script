ab781af23008a05cff9845ce856e56d5ce85d204
sakhawathossensadi <newtonss919@gmail.com>
2022-10-11 01:20:10 +0600

    JS date methods

---
+++ JS/GetDateMethods.js
@@ -0,0 +1,9 @@
+const dateTime = new Date();
+console.log("Year : ",dateTime.getFullYear());
+console.log("Month : ",dateTime.getMonth());
+console.log("Date : ",dateTime.getDate());
+console.log("Hour : ",dateTime.getHours());
+console.log("Minutes : ",dateTime.getMinutes());
+console.log("Seconds : ",dateTime.getSeconds());
+console.log("Seconds : ",dateTime.getSeconds());
+console.log("Day : ",dateTime.getDay()); // sunday is week first day
