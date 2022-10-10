7e61aa26000f02a1257856596858110c430416e2
sakhawathossensadi <newtonss919@gmail.com>
2022-10-11 00:55:57 +0600

    JS Date

---
+++ JS/Date.js
@@ -0,0 +1,6 @@
+const date = new Date();
+console.log("Date : ", date);
+console.log("Date : ", date.toString());
+console.log("Date : ", date.toDateString());
+console.log("Date : ", date.toUTCString());
+console.log("Date : ", date.toISOString());
